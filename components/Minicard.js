62466ed7c063b848cd060dd259df3697db009dc9
H41ch <h41ch5ss@gmail.com>
2024-12-29 21:46:24 +0000

    Update Minicard.js

--- components/Minicard.js
+++ components/Minicard.js
@@ -1,7 +1,9 @@
+import PropTypes from 'prop-types';
+
 function Minicard(props) {
   return (
     <div className="Minicard">
-      <img src={props.src} alt="" className="Minicard-icon" />
+      <img src={props.src} alt={props.alt || 'Minicard icon'} className="Minicard-icon" />
       <div className="Minicard-content">
         <h4 className="Minicard-content-title">{props.title}</h4>
         <p className="Minicard-content-text">{props.text}</p>
@@ -9,5 +11,16 @@ function Minicard(props) {
     </div>
   );
 }
+
+Minicard.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+Minicard.defaultProps = {
+  alt: 'Minicard icon',
+};
 
 export default Minicard;
