fd9213f2a151bad879a83d7a759bb4ea2ba56909
H41ch <h41ch5ss@gmail.com>
2024-12-29 21:31:48 +0000

    Update Customercard.js

--- components/Customercard.js
+++ components/Customercard.js
@@ -1,15 +1,23 @@
-function Customercard(props) {
+import PropTypes from 'prop-types';
+
+function Customercard({ text, img, name }) {
   return (
     <div className="Customercard">
       <div className="Customercard-top">
-        <p className="Customercard-top-text">{props.text}</p>
+        <p className="Customercard-top-text">{text}</p>
       </div>
       <div className="Customercard-bottom">
-        <img src={props.img} alt="" className="Customercard-bottom-profile" />
-        <span className="Customercard-bottom-name">{props.name}</span>
+        <img src={img} alt="Customer profile" className="Customercard-bottom-profile" />
+        <span className="Customercard-bottom-name">{name}</span>
       </div>
     </div>
   );
 }
+
+Customercard.propTypes = {
+  text: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 export default Customercard;
