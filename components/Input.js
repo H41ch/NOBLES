fa550135cfea550e24af3f74de548de6c4bc2da8
H41ch <h41ch5ss@gmail.com>
2024-12-29 21:35:28 +0000

    Update Input.js

--- components/Input.js
+++ components/Input.js
@@ -1,13 +1,37 @@
-function Input(props) {
+import PropTypes from 'prop-types';
+
+function Input({ type, name, id, className, placeholder, value, onChange }) {
   return (
     <input
-      type="text"
-      name=""
-      id=""
-      className="Input"
-      placeholder={props.placeholder}
+      type={type}
+      name={name}
+      id={id}
+      className={className}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
     />
   );
 }
+
+Input.propTypes = {
+  type: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  className: PropTypes.string,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+Input.defaultProps = {
+  type: 'text',
+  name: '',
+  id: '',
+  className: 'Input',
+  placeholder: '',
+  value: '',
+  onChange: () => {},
+};
 
 export default Input;
