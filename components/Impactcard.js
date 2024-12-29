99d8f5f121707c77d864df57cd4c67942f3569df
H41ch <h41ch5ss@gmail.com>
2024-12-29 21:34:28 +0000

    Update Impactcard.js

--- components/Impactcard.js
+++ components/Impactcard.js
@@ -1,15 +1,23 @@
-function Impactcard(props) {
+import PropTypes from 'prop-types';
+
+function Impactcard({ img, title, description }) {
   return (
     <div className="Impactcard">
       <div className="Impactcard-left">
-        <img src={props.img} alt="" className="Impactcard-left-img" />
+        <img src={img} alt={title} className="Impactcard-left-img" />
       </div>
       <div className="Impactcard-right">
-        <div className="Impactcard-right-title">{props.title}</div>
-        <div className="Impactcard-right-description">{props.description}</div>
+        <div className="Impactcard-right-title">{title}</div>
+        <div className="Impactcard-right-description">{description}</div>
       </div>
     </div>
   );
 }
+
+Impactcard.propTypes = {
+  img: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export default Impactcard;
